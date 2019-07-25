4c3f54af8d6a91c95e626c220bba227512a5fd91
EC2 Default User <ec2-user@ip-172-31-38-95.us-east-2.compute.internal>
2019-07-25 12:37:50 +0000

    Change views

--- app/assets/javascripts/internships.js
+++ app/assets/javascripts/internships.js
@@ -1,19 +1,17 @@
-<script>
-    function over(obj) {
-        $(obj).find('.vuc').css('color', 'lightskyblue');
-    }
+function over(obj) {
+	$(obj).find('.vuc').css('color', 'lightskyblue');
+}
 
-    function out(obj) {
-        $(obj).find('.vuc').css('color', 'black');
-    }
+function out(obj) {
+	$(obj).find('.vuc').css('color', 'black');
+}
 
-    function clk(i) {
-        if (i == 0) {
-            $('#std').addClass('active');
-            $('#intern').removeClass('active');
-        } else if (i == 1) {
-            $('#std').removeClass('active');
-            $('#intern').addClass('active');
-        }
-    }
-</script>
+function clk(i) {
+	if (i == 0) {
+		$('#std').addClass('active');
+		$('#intern').removeClass('active');
+	} else if (i == 1) {
+		$('#std').removeClass('active');
+		$('#intern').addClass('active');
+	}
+}
