baad7178a777976b677260247c0614a6d92e679d
cb50one <txmalon0628@gmail.com>
2019-07-27 17:29:00 +0900

    display events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,9 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.page(params[:page]).per(20)
+  end
+
+  def show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'internships#index'
   get 'internships/show'
+  get 'events/index'
+  get 'events/show'
 end
