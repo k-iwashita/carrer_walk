cbf118360ae26b8227570c7045a2a2ab492f78a0
tomotomy <tomo1999724@gmail.com>
2019-09-12 06:59:55 +0000

    refine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_051425) do
+ActiveRecord::Schema.define(version: 2019_09_12_055729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,16 @@ ActiveRecord::Schema.define(version: 2019_09_07_051425) do
     t.string "detail", limit: 10000
     t.datetime "publish_start_at"
     t.datetime "publish_end_at"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "upload_files", force: :cascade do |t|
+    t.string "default_file_name", limit: 200
+    t.string "managed_file_name", limit: 200
+    t.string "fileable_type"
+    t.bigint "fileable_id"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
