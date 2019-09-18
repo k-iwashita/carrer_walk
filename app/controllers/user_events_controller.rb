d31d62c84c32931fa86c6b8ddc4ca974568562d8
tomotomy <tomo1999724@gmail.com>
2019-09-18 10:21:57 +0000

    fixed error like >>>>>>>>HEAD

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -7,22 +7,11 @@ class UserEventsController < ApplicationController
     redirect_to event_path(@event)
   end 
 
-<<<<<<< HEAD
   def destroy
     @user_event = UserEvent.find(params[:id])
     @event = Event.find_by(id: @user_event.event_id)
     @user_event.destroy
     redirect_to event_path(@event)
-  
-=======
-  def delete
-    @user = current_user
-    @event = Event.find(params[:id])
-    @user_event = @user.user_events.find(event_id: @event.id)
-    @user_event.destroy
-    redirect_to events_path
->>>>>>> d223c1ed20a8f19b26c94a4f7ed05d044f03c7d0
   end 
-
 
 end
