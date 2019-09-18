21cccd598b80819e61ada2e01ff6087a2fff4c18
tomotomy <tomo1999724@gmail.com>
2019-09-18 10:07:23 +0000

    implement participate btn and fix git error

---
+++ app/controllers/user_events_controller.rb
@@ -0,0 +1,19 @@
+class UserEventsController < ApplicationController 
+   before_action :authenticate_user! ,only: [:delete, :create]
+  def create
+    @user = current_user
+    @event = Event.find(params[:event_id])
+    @user.user_events.create(event_id: @event.id)
+    redirect_to event_path(@event)
+  end 
+
+  def destroy
+    @user_event = UserEvent.find(params[:id])
+    @event = Event.find_by(id: @user_event.event_id)
+    @user_event.destroy
+    redirect_to event_path(@event)
+  
+  end 
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'internships#index'
   get 'internships/show'
 
+
   resources :events
   resources :jobs
   resources :lessons
+  resources :user_events, only: [:create, :destroy]
 end
