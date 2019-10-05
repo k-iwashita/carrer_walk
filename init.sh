c9e5ac2bf2b8870815bc1369af50df4b067ee94c
Reinker <knaoto0223@yahoo.co.jp>
2019-10-05 08:23:36 +0000

    remove sh files

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker-compose build
-docker-compose run web rake db:create
-docker-compose run web rails runner db/schema.rb
-docker-compose run web rails runner lib/event.rb
-docker-compose run web rails runner lib/internship.rb
-docker-compose up
