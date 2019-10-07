1a000721bb64a13f36ccacd3a7f9f54e9b324126
tomotomy <tomo1999724@gmail.com>
2019-10-07 10:21:21 +0000

    omniauth twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   validates :name, presence: true
   validates :email, presence: true 
@@ -14,5 +14,30 @@ class User < ApplicationRecord
   has_many :lessons, through: :user_lessons
 
   has_many :user_events, dependent: :destroy
-  has_many :events, through: :user_events
-end
+  has_many :events, through: :user_events 
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.id, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        email: User.dummy_email(auth),
+        password: Devise.friendly_token[0,20],
+        image: auth.info.image,
+        name: auth.info.name,
+        nickname: auth.info.nickname,
+      )
+    end 
+
+    user 
+  end  
+
+  private 
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end 
+
+end 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, 'zkCKALMgGvBgRGKS0OSRBZch5', 'BkE4bl0k2mADhr2WHdgH9T5ZspbrdKvdsfqIx1pRtMmP7vv4y0', callback_url: "https://localhost:3000/users/auth/twitter/callback"
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
@@ -296,4 +298,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  
+
 end
