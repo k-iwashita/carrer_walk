051ef805752b8d7d16a46d2d8285b76bc4be6ad5
tomotomy <tomo1999724@gmail.com>
2019-10-16 07:32:32 +0000

    create new events index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-  root 'internships#index'
+  root 'events#index'
   get 'internships/:id', to: 'internships#show'
 
 
