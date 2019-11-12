c90f67bec16b2216e38cfcefdd6e42dd69838070
Reinker <knaoto0223@yahoo.co.jp>
2019-11-13 03:42:47 +0900

    change shell command

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 docker-compose build
 docker-compose run web rake db:create
-chown -R $USER:$USER .
+echo Execute db/schema.rb ...
 docker-compose run web rails runner db/schema.rb
+echo Done!
+echo Execute lib/event.rb ...
 docker-compose run web rails runner lib/event.rb
+echo Done!
+echo Execute lib/internship.rb
 docker-compose run web rails runner lib/internship.rb
-docker-compose up
+echo Done!
+chown -R $USER:$USER .
